fd6769dd499d77c75cfedab02f020c6fdd0a2172
Andy Chen <andycjx8@gmail.com>
2021-08-13 11:39:42 +1000

    add REST API for genre

--- routers/genres.js
+++ routers/genres.js
@@ -11,11 +11,56 @@ const valiadateGenre = (genre) => {
   return schema.validate(genre);
 };
 
+const genres = [
+  {
+    id: 1,
+    name: "History",
+  },
+  {
+    id: 2,
+    name: "Action",
+  },
+  {
+    id: 3,
+    name: "Thriller",
+  },
+];
+
 router.post("/", validate(valiadateGenre), (req, res) => {
   const genre = {
     name: req.body.name,
   };
+
+  genres.push(genre);
+  res.send(genres);
+});
+
+router.get("/", (req, res) => {
+  res.send(genres);
+});
+
+router.get("/:id", (req, res) => {
+  const genre = genres.find((g) => g.id === parseInt(req.params.id));
+  if (!genre)
+    return res.status(404).send("The genre with given id cannot be found");
+
   res.send(genre);
+});
+
+router.put("/:id", validate(valiadateGenre), (req, res) => {
+  const genre = genres.find((g) => g.id === parseInt(req.params.id));
+  if (!genre)
+    return res.status(404).send("The genre with given id cannot be found");
+
+  genre.name = req.body.name;
+
+  res.send(genre);
+});
+
+router.delete("/:id", (req, res) => {
+  const new_genres = genres.filter((g) => g.id !== parseInt(req.params.id));
+  res.send(new_genres);
+  //404
 });
 
 module.exports = router;
