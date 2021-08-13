116969a2b368001a7bac92ade7dc92c4d9fa97a9
Andy Chen <andycjx8@gmail.com>
2021-08-13 14:30:38 +1000

    set up mongoose connection

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const genres = require("./routers/genres");
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+
+mongoose
+  .connect("mongodb://localhost/store", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected with database..."))
+  .catch((err) => console.log(err, "Fail to connect with database..."));
 
 app.use(express.json());
 
