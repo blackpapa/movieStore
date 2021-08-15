5657380aa127cf587e5a80dbedca5d14bae9c4d3
Andy Chen <andycjx8@gmail.com>
2021-08-16 09:16:00 +1000

    Build the POST API for movie

--- routers/movies.js
+++ routers/movies.js
@@ -1,4 +1,20 @@
+const validate = require("../middlewares/validate");
+const { validateMovie, Movie } = require("../models/movie");
+const { Genre } = require("../models/genre");
 const express = require("express");
 const router = express.Router();
+
+router.post("/", validate(validateMovie), async (req, res) => {
+  const genre = await Genre.findById(req.body.genreId);
+  const movie = new Movie({
+    title: req.body.title,
+    genre: genre,
+    numberInStock: req.body.numberInStock,
+    dailyRentalRate: req.body.dailyRentalRate,
+  });
+
+  await movie.save();
+  res.send(movie);
+});
 
 module.exports = router;
