930d487d6f1fe95643981220310a61454f30d06f
Andy Chen <andycjx8@gmail.com>
2021-08-17 09:53:35 +1000

    Build POST API for user router

--- routers/users.js
+++ routers/users.js
@@ -5,7 +5,14 @@ const router = express.Router();
 require("express-async-errors");
 
 router.post("/", validate(validateUser), async (req, res) => {
-  const user = new User({
+  //400
+  let user = await User.findOne({
+    email: req.body.email,
+  });
+
+  if (user) return res.status(400).send("The user is already existed");
+
+  user = new User({
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
