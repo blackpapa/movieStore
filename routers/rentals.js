39b6a4a31bd520cfe692b26afa35ad3bda13935e
Andy Chen <andycjx8@gmail.com>
2021-08-16 17:46:00 +1000

    Build post API for rental

--- routers/rentals.js
+++ routers/rentals.js
@@ -1,7 +1,25 @@
-const express = require("express");
 const { Customer } = require("../models/customer");
+const { Movie } = require("../models/movie");
+const { Rental, validateRental } = require("../models/rental");
+const _ = require("lodash");
+const validate = require("../middlewares/validate");
+const express = require("express");
 const router = express.Router();
 
-router.post("/", async (req, res) => {});
+router.post("/", validate(validateRental), async (req, res) => {
+  const customer = await Customer.findById(req.body.customerId);
+  if (!customer) return res.status(400).send("Invalid customer id");
+
+  const movie = await Movie.findById(req.body.movieId);
+  if (!movie) return res.status(400).send("Invalid movie id");
+
+  const rental = new Rental({
+    customer: _.pick(customer, ["name", "phone", "isGold"]),
+    movie: _.pick(movie, ["title", "dailyRentalRate"]),
+  });
+
+  await rental.save();
+  res.send(rental);
+});
 
 module.exports = router;
