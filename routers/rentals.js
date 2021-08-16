6e962555db7292a9892eca0b2b30aee4111ae5af
Andy Chen <andycjx8@gmail.com>
2021-08-17 08:41:28 +1000

    Build GET API for rental

--- routers/rentals.js
+++ routers/rentals.js
@@ -1,8 +1,9 @@
 const { Customer } = require("../models/customer");
 const { Movie } = require("../models/movie");
 const { Rental, validateRental } = require("../models/rental");
-const _ = require("lodash");
+const validateObjectId = require("../middlewares/validateObjectId");
 const validate = require("../middlewares/validate");
+const _ = require("lodash");
 const express = require("express");
 const router = express.Router();
 
@@ -19,6 +20,21 @@ router.post("/", validate(validateRental), async (req, res) => {
   });
 
   await rental.save();
+  res.send(rental);
+});
+
+router.get("/", async (req, res) => {
+  const rentals = await Rental.find();
+  if (!rentals) return res.status(404).send("No rental in the database");
+
+  res.send(rentals);
+});
+
+router.get("/:id", validateObjectId, async (req, res) => {
+  const rental = await Rental.findById(req.params.id);
+  if (!rental)
+    return res.status(404).send("The rental with given id cannot be found");
+
   res.send(rental);
 });
 
