c6de30a75634447927886b6ee2452ab7af5f57c1
Andy Chen <andycjx8@gmail.com>
2021-08-16 17:35:47 +1000

    Set up rental router

---
+++ routers/rentals.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const { Customer } = require("../models/customer");
+const router = express.Router();
+
+router.post("/", async (req, res) => {});
+
+module.exports = router;
