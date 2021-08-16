3f90e1447277fa02170b0f7164b8ee0c31dbc26c
Andy Chen <andycjx8@gmail.com>
2021-08-17 09:40:30 +1000

    add string() to email in user validator schema

--- models/user.js
+++ models/user.js
@@ -12,7 +12,7 @@ const User = mongoose.model("User", userSchema);
 const validateUser = (user) => {
   const schema = Joi.object({
     name: Joi.string().min(2).max(50).required(),
-    email: Joi.email().min(5).max(50).required(),
+    email: Joi.string().min(5).max(50).email().required(),
     password: Joi.string().min(5).max(1024).required(),
   });
 
--- routers/users.js
+++ routers/users.js
@@ -1,5 +1,18 @@
+const { validateUser, User } = require("../models/user");
+const validate = require("../middlewares/validate");
 const express = require("express");
 const router = express.Router();
 require("express-async-errors");
+
+router.post("/", validate(validateUser), async (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  await user.save();
+  res.send(user);
+});
 
 module.exports = router;
