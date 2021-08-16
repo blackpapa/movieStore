8523a7d4a98d428b00af41375038da665420e11a
Andy Chen <andycjx8@gmail.com>
2021-08-16 17:35:15 +1000

    Update rental model with embeded document

--- models/rental.js
+++ models/rental.js
@@ -2,8 +2,32 @@ const mongoose = require("mongoose");
 const Joi = require("joi");
 
 const rentalSchema = new mongoose.Schema({
-  customerId: { type: String, minLength: 2, maxLength: 255, required: true },
-  movieId: { type: String, minLength: 2, maxLength: 255, required: true },
+  customer: {
+    type: new mongoose.Schema({
+      name: { type: String, minLength: 2, maxLength: 255, required: true },
+      phone: { type: String, minLength: 2, maxLength: 20, required: true },
+      isGold: { type: Boolean, default: false },
+    }),
+    required: true,
+  },
+  movie: {
+    type: new mongoose.Schema({
+      title: {
+        type: String,
+        minLength: 2,
+        maxLength: 255,
+        required: true,
+      },
+      dailyRentalRate: {
+        type: Number,
+        min: 0,
+        max: 255,
+        required: true,
+      },
+    }),
+    required: true,
+  },
+  dateOut: { type: Date, default: Date.now() },
 });
 
 const Rental = mongoose.model("Rental", rentalSchema);
