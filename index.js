dfeea2e6f2c76ce3183815319710bd94ab0d26a3
Andy Chen <andycjx8@gmail.com>
2021-08-17 09:34:12 +1000

    Set up user router

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const movies = require("./routers/movies");
 const customers = require("./routers/customers");
 const rentals = require("./routers/rentals");
 const error = require("./middlewares/error");
+const users = require("./routers/users");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const mongoose = require("mongoose");
@@ -25,6 +26,7 @@ app.use("/api/genres", genres);
 app.use("/api/movies", movies);
 app.use("/api/customers", customers);
 app.use("/api/rentals", rentals);
+app.use("/api/users", users);
 app.use(error);
 
 const port = process.env.PORT || 3000;
---
+++ routers/users.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const router = express.Router();
+require("express-async-errors");
+
+module.exports = router;
