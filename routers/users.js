dc5cefa306978f08f0038b0f14a6cdceda4550f6
Andy Chen <andycjx8@gmail.com>
2021-08-17 10:04:13 +1000

    Hash password with bcrypt

--- routers/users.js
+++ routers/users.js
@@ -1,5 +1,7 @@
 const { validateUser, User } = require("../models/user");
 const validate = require("../middlewares/validate");
+const bcrypt = require("bcrypt");
+const _ = require("lodash");
 const express = require("express");
 const router = express.Router();
 require("express-async-errors");
@@ -12,11 +14,10 @@ router.post("/", validate(validateUser), async (req, res) => {
 
   if (user) return res.status(400).send("The user is already existed");
 
-  user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  });
+  user = new User(_.pick(req.body, ["name", "email", "password"]));
+
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
 
   await user.save();
   res.send(user);
