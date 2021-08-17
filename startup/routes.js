33c21471efc7c42c6f9f120d22af2375881e520f
Andy Chen <andycjx8@gmail.com>
2021-08-18 09:50:48 +1000

    extract routers from index

---
+++ startup/routes.js
@@ -0,0 +1,20 @@
+const genres = require("../routers/genres");
+const movies = require("../routers/movies");
+const customers = require("../routers/customers");
+const rentals = require("../routers/rentals");
+const auth = require("../routers/auth");
+const error = require("../middlewares/error");
+const users = require("../routers/users");
+const express = require("express");
+
+module.exports = function (app) {
+  app.use(express.json());
+
+  app.use("/api/genres", genres);
+  app.use("/api/movies", movies);
+  app.use("/api/customers", customers);
+  app.use("/api/rentals", rentals);
+  app.use("/api/users", users);
+  app.use("/api/auth", auth);
+  app.use(error);
+};
