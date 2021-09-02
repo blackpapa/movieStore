9cf8a72deb5d36e24487bffc50fe643eeeaf025d
Andy Chen <andycjx8@gmail.com>
2021-09-02 11:41:19 +1000

    Update port to 3900

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require("./startup/config")();
 require("./startup/routes")(app);
 require("./startup/db")();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3900;
 var server = app.listen(port, logger.info(`Listening on port ${port}...`));
 
 module.exports = server;
