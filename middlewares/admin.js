2b31767f903be2f9de15f4bc2bb8842f6c5b306b
Andy Chen <andycjx8@gmail.com>
2021-09-02 13:08:29 +1000

    Disable admin

--- middlewares/admin.js
+++ middlewares/admin.js
@@ -1,4 +1,7 @@
+const config = require("config");
+
 module.exports = (req, res, next) => {
+  if (!config.get("requireAuth")) return next();
   if (!req.user.isAdmin)
     return res.status(403).send("Unauthorized user. Forbidden operation");
 
