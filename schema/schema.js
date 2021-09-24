87c5c13aa8a853e82980ea484cec239762699ab5
Andy Chen <andycjx8@gmail.com>
2021-09-24 10:33:38 +1000

    Add genre to root query

--- schema/schema.js
+++ schema/schema.js
@@ -5,6 +5,7 @@ const {
   GraphQLBoolean,
   GraphQLSchema,
 } = require("graphql");
+const { Genre } = require("../models/genre");
 
 const movieType = new GraphQLObjectType({
   name: "Movie",
@@ -17,6 +18,14 @@ const movieType = new GraphQLObjectType({
   }),
 });
 
+const genreType = new GraphQLObjectType({
+  name: "Genre",
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+  }),
+});
+
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
@@ -25,6 +34,13 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLString } },
       resolve(parent, args) {
         //code to get data from database
+      },
+    },
+    genre: {
+      type: genreType,
+      args: { id: { type: GraphQLString } },
+      async resolve(parent, args) {
+        await Genre.findById(args.id);
       },
     },
   },
