a9a1a018fc4bb372759e04d62ec17b6232157b78
Andy Chen <andycjx8@gmail.com>
2021-09-27 09:09:54 +1000

    Update the name

--- schema/schema.js
+++ schema/schema.js
@@ -11,25 +11,25 @@ const { Movie } = require("../models/movie");
 const { Customer } = require("../models/customer");
 const { Rental } = require("../models/rental");
 
-const movieType = new GraphQLObjectType({
+const MovieType = new GraphQLObjectType({
   name: "Movie",
   fields: () => ({
     id: { type: GraphQLString },
     title: { type: GraphQLString },
-    genre: { type: genreType },
+    genre: { type: GenreType },
     numberInStock: { type: GraphQLInt },
     dailyRentalRate: { type: GraphQLInt },
     liked: { type: GraphQLBoolean },
   }),
 });
 
-const genreType = new GraphQLObjectType({
+const GenreType = new GraphQLObjectType({
   name: "Genre",
   fields: () => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
     movies: {
-      type: new GraphQLList(movieType),
+      type: new GraphQLList(MovieType),
       async resolve(parent, args) {
         const movies = await Movie.find();
         return movies.filter((m) => m.genre._id.toString() === parent.id);
@@ -38,7 +38,7 @@ const genreType = new GraphQLObjectType({
   }),
 });
 
-const customerType = new GraphQLObjectType({
+const CustomerType = new GraphQLObjectType({
   name: "Customer",
   fields: () => ({
     id: { type: GraphQLString },
@@ -46,7 +46,7 @@ const customerType = new GraphQLObjectType({
     isGold: { type: GraphQLBoolean },
     phone: { type: GraphQLString },
     rentals: {
-      type: new GraphQLList(rentalType),
+      type: new GraphQLList(RentalType),
       async resolve(parent, args) {
         const rentals = await Rental.find();
         return rentals.filter((r) => r.customer._id.toString() === parent.id);
@@ -55,12 +55,12 @@ const customerType = new GraphQLObjectType({
   }),
 });
 
-const rentalType = new GraphQLObjectType({
+const RentalType = new GraphQLObjectType({
   name: "Rental",
   fields: () => ({
     id: { type: GraphQLString },
-    customer: { type: customerType },
-    movie: { type: movieType },
+    customer: { type: CustomerType },
+    movie: { type: MovieType },
     dateOut: { type: GraphQLString },
     dateReturn: { type: GraphQLString },
     rentalFee: { type: GraphQLInt },
@@ -72,7 +72,7 @@ const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
     movie: {
-      type: movieType,
+      type: MovieType,
       args: { id: { type: GraphQLString } },
       async resolve(parent, args) {
         //code to get data from database
@@ -80,46 +80,46 @@ const RootQuery = new GraphQLObjectType({
       },
     },
     genre: {
-      type: genreType,
+      type: GenreType,
       args: { id: { type: GraphQLString } },
       async resolve(parent, args) {
         return await Genre.findById(args.id);
       },
     },
     customer: {
-      type: customerType,
+      type: CustomerType,
       args: { id: { type: GraphQLString } },
       async resolve(parent, args) {
         return await Customer.findById(args.id);
       },
     },
     rental: {
-      type: rentalType,
+      type: RentalType,
       args: { id: { type: GraphQLString } },
       async resolve(parent, args) {
         return await Rental.findById(args.id);
       },
     },
     movies: {
-      type: new GraphQLList(movieType),
+      type: new GraphQLList(MovieType),
       async resolve(parent, args) {
         return await Movie.find();
       },
     },
     genres: {
-      type: new GraphQLList(genreType),
+      type: new GraphQLList(GenreType),
       async resolve(parent, args) {
         return await Genre.find();
       },
     },
     customers: {
-      type: new GraphQLList(customerType),
+      type: new GraphQLList(CustomerType),
       async resolve(parent, args) {
         return await Customer.find();
       },
     },
     rentals: {
-      type: new GraphQLList(rentalType),
+      type: new GraphQLList(RentalType),
       async resolve(parent, args) {
         return await Rental.find();
       },
