773a162190ca040312f84571d7ab22ba404614c1
Andy Chen <andycjx8@gmail.com>
2021-09-28 14:13:38 +1000

    update

--- startup/graphql.js
+++ startup/graphql.js
@@ -2,5 +2,5 @@ const schema = require("../schema/schema");
 const { graphqlHTTP } = require("express-graphql");
 
 module.exports = function (app) {
-  app.use("/graphql", graphqlHTTP({ schema, graphiql: false }));
+  app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
 };
